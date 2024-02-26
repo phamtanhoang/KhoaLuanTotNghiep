3b47f986db6aa742630fcbf69f87ff2275a488f9
phamtanhoang <88889770+phamtanhoang@users.noreply.github.com>
2024-02-26 23:33:54 +0700

    no message

--- task-backend/src/main/java/com/pth/taskbackend/service/BootstrapService.java
+++ task-backend/src/main/java/com/pth/taskbackend/service/BootstrapService.java
@@ -1,8 +1,11 @@
 package com.pth.taskbackend.service;
 
+import com.pth.taskbackend.enums.ERole;
 import com.pth.taskbackend.enums.EStatus;
+import com.pth.taskbackend.model.meta.Account;
 import com.pth.taskbackend.model.meta.Role;
 import com.pth.taskbackend.model.meta.User;
+import com.pth.taskbackend.repository.AccountRepository;
 import com.pth.taskbackend.repository.RoleRepository;
 import com.pth.taskbackend.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
@@ -21,6 +24,7 @@ public class BootstrapService {
     private final UserRepository appUserRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AccountRepository accountRepository;
 
     @PostConstruct
     public void bootstrap() {
@@ -40,6 +44,16 @@ public class BootstrapService {
                     Set.of(userRole)
             );
             this.appUserRepository.save(user);
+        }
+
+        String email2 = "admin@example.com";
+        if (accountRepository.findByEmail(email2).isEmpty()) {
+            Account account = new Account();
+            account.setEmail(email2);
+            account.setPassword(passwordEncoder.encode("password"));
+            account.setStatus(EStatus.ACTIVE);
+            account.setRole(ERole.ADMIN);
+            this.accountRepository.save(account);
         }
     }
 }
