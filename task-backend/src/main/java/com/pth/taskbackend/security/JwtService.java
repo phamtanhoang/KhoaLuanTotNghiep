469863ec6c0afe3d372fb917795dae61565b6865
DAT\ACER <goneanhem@gmail.com>
2024-04-07 15:57:36 +0700

    Reapply "3:55pm 4/7/2024"
    This reverts commit 6fe09d5cf4138bee96db3bc3a546b16ca3f90000.

---
+++ task-backend/src/main/java/com/pth/taskbackend/security/JwtService.java
@@ -0,0 +1,83 @@
+package com.pth.taskbackend.security;
+
+import com.pth.taskbackend.enums.EStatus;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.*;
+import java.util.function.Function;
+
+@Component
+public class JwtService {
+
+    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+
+    private List<String> validTokens = new ArrayList<>();
+    public String generateToken(String username, EStatus status) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username, status);
+    }
+
+    private String createToken(Map<String, Object> claims, String username,EStatus status) {
+        String token = Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .claim("status",status)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 90))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+        validTokens.add(token);
+        return token;
+
+
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes= Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public boolean isTokenValid(String token) {
+        return validTokens.contains(token);
+    }
+
+    public void invalidateToken(String token) {
+        validTokens.remove(token);
+    }
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+}
